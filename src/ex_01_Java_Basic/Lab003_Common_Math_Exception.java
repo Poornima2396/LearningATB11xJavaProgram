75d2f87ab2c54fb25b26be2bbd31cf2ada010820
Dell Inspiron <poornima.kamble23sep@gmail.com>
2025-03-10 11:38:12 +0530

    10th march, I am pushing Println vs Print java code

---
+++ src/ex_01_Java_Basic/Lab003_Common_Math_Exception.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Common_Math_Exception {
+    public static void main(String[] args) {
+        System.out.println(10/0); //.ArithmeticException / by zero
+    }
+
+}
---
+++ src/ex_01_Java_Basic/Lab004_Println_vs_Print.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Println_vs_Print {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");  //print with new line
+        System.out.println("HelloWorld2"); //print with new line
+
+        System.out.print("HelloWorld4"); //not in new line
+        System.out.print("HelloWorld5"); //not in new line
+    }
+
+}

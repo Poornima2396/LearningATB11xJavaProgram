177a159bac152988c180d36606f8a6f1527620b8
Dell Inspiron <poornima.kamble23sep@gmail.com>
2025-03-12 16:12:36 +0530

    11th March, Java assignment

---
+++ src/ex_02_Java_Basic2/Lab017_Table_9_Assignment.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basic2;
+
+public class Lab017_Table_9_Assignment {
+    public static void main(String[] args) {
+        int table=9;
+        System.out.printf("%d x 1 = %d\n",table,table*1);
+        System.out.printf("%d x 2 = %d\n",table,table*2);
+        System.out.printf("%d x 3 = %d\n",table,table*3);
+        System.out.printf("%d x 4 = %d\n",table,table*4);
+        System.out.printf("%d x 5 = %d\n",table,table*5);
+        System.out.printf("%d x 6 = %d\n",table,table*6);
+        System.out.printf("%d x 7 = %d\n",table,table*7);
+        System.out.printf("%d x 8 = %d\n",table,table*8);
+        System.out.printf("%d x 9 = %d\n",table,table*9);
+        System.out.printf("%d x 10 = %d",table,table*10);
+    }
+
+}
